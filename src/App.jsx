e4e8f8afaf2a7919389c107fec7551ba5c712256
Matheus albuquerque <matheus.2020110153@unicap.br>
2023-05-04 22:03:59 -0300

    Estilizando

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ const App = () =>{
 
   return (
     <div className="App">
+      <h1>Unibar</h1>
       {drinks.map((drink) =>{
         return <Card drink={drink}/>;
       })}
--- src/Components/card.component.jsx
+++ src/Components/card.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import "./card.styles.css"
 
 const Card = ({drink}) => {
 
     const{id,description,name,imageLink,graduacaoAlcoolica} = drink;
 
   return (
-    <div key={id}>
+    <div className='drink-container' key={id}>
         <img src={imageLink} alt={`foto da bebida ${name}`}/>
         <h2>{name}</h2>
         <p>{description}</p>
